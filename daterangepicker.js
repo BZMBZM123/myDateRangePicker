623950a7739e43b1ec071d26b167c26c7ea55aa7
BZMBZM123 <445826051@qq.com>
2017-09-05 19:00:25 +0800

    测试提交

--- daterangepicker.js
+++ daterangepicker.js
@@ -26,6 +26,7 @@
     root.daterangepicker = factory(root.moment, root.jQuery);
   }
 }(this, function(moment, $) {
+  //aa
   window.$ = $;
   var DateRangePicker = function(element,scopeName, options, cb) {
     //default settings for options
